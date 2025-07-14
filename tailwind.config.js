fb6852125f488a830f44ebce0a8501a66f170b04
kim400im <min2000jae@naver.com>
2025-07-14 14:58:26 +0900

    shadcn 구성, 로그인 로그아웃

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // shadcn은 class 기반 다크모드 사용
+  plugins: [],
+};
